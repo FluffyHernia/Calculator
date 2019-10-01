870498b8b04375dd93a891611d40cf708a7f9d1c
SarahMars <sarah@frl.mech.tohoku.ac.jp>
2019-10-01 21:41:59 +0900

    Added calculating function

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Management.Automation;
+
+
 
 namespace Calculator
 {
@@ -20,6 +24,8 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool press_eq = false;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,13 +33,54 @@ namespace Calculator
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //textBox.Text += "0";
+            if (press_eq == true){
+                textBox.Text = string.Empty;
+                press_eq = false;
+            }
             textBox.Text += (sender as Button).Content;
         }
 
-        //private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        //{
+        private void Button_Click_times(object sender, RoutedEventArgs e)
+        {
+            textBox.Text += "*";
+        }
 
-        //}
+
+        //  現時点でtextBoxに入っている計算をPowerShellに投げる
+        private void Button_Click_equal(object sender, RoutedEventArgs e)
+        {
+            string input = @textBox.Text;
+            string output;
+
+            using (var invoker = new RunspaceInvoke())
+            {
+                var result = invoker.Invoke(input);
+
+                //output = result.ToString();
+                press_eq = true;
+
+                /*You can't pass collection of PSObject to datasource directly. 
+                  You need to retrieve relating data and regroup it as right format that datagridview recognized
+                */            
+                output = result[0].ToString();
+
+                textBox.Text += "\r\n = " + output;
+
+            }
+            //object result = invoker.Invoke(input)[0]
+
+            //input.Replace('1', '2');
+
+            //textBox.Text += input.ToString();
+
+            //Process cmd = new Process();
+            //cmd.StartInfo.FileName = "PowerShell.exe";
+            //// PowerShellのWindowを立ち上げずに実行
+            //cmd.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            // 
+
+        }
+
     }
 }
