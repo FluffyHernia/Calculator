ae5dd20b067658ef43937b411f5a4d3e990ab5f8
SarahMars <sarah@frl.mech.tohoku.ac.jp>
2019-10-05 18:42:34 +0900

    ボタンクリックのイベントハンドラを一つの関数に統合

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace Calculator
     public partial class MainWindow : Window
     {
         bool press_eq = false;
+        string prev_ans = ""; // 前回の値を記録する
 
         public MainWindow()
         {
@@ -33,40 +34,51 @@ namespace Calculator
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (press_eq == true){
-                textBox.Text = string.Empty;
-                press_eq = false;
+            if (((Button)sender).Content.ToString() == "DEL") // deleteキーが押されたとき
+            {
+                delete(sender, e);
             }
-            textBox.Text += (sender as Button).Content;
+            else if (((Button)sender).Content.ToString() == "x") // かけるが押されたとき*で表示
+            {
+                textBox.Text += "*";
+            }
+            else if (((Button)sender).Content.ToString() == "AC") // all-clear
+            {
+                textBox.Text = "";
+            }
+            else if (((Button)sender).Content.ToString() == "=") {
+                equal(sender, e);
+            }
+            else
+            {
+                if (press_eq == true)
+                {
+                    textBox.Text = string.Empty;
+                    press_eq = false;
+                }
+                textBox.Text += (sender as Button).Content;
+
+            }
         }
 
-        private void Button_Click_times(object sender, RoutedEventArgs e)
+        private void delete(object sender, RoutedEventArgs e)
         {
-            textBox.Text += "*";
-        }
-
-        private void Button_Click_delete(object sender, RoutedEventArgs e)
-        {
-            //textBox.Text += (sender as Button).Content;
             string text_delete = textBox.Text;
             if (text_delete.Length > 1)
             {
                 text_delete = text_delete.Substring(0, text_delete.Length - 1); // 最後の一文字を消す
             }
-            else {
+            else
+            {
                 text_delete = "";
             }
 
             textBox.Text = text_delete;
         }
 
-        private void Button_Click_allclear(object sender, RoutedEventArgs e)
-        {
-            textBox.Text = "";
-        }
 
         //  現時点でtextBoxに入っている計算をPowerShellに投げる
-        private void Button_Click_equal(object sender, RoutedEventArgs e)
+        private void equal(object sender, RoutedEventArgs e)
         {
             string input = @textBox.Text;
             string output;
@@ -84,7 +96,6 @@ namespace Calculator
                 output = result[0].ToString();
 
                 textBox.Text += "\r\n = " + output;
-
             }
             //object result = invoker.Invoke(input)[0]
 
